6404d0e6fc00ffb9cf7acb23fdaa67a52eafea0a
jhwang <5832120@naver.com>
2024-05-08 00:25:12 +0900

    delete spaces

--- src/GuiWrapper.cpp
+++ src/GuiWrapper.cpp
@@ -12,7 +12,7 @@ void GuiWrapper::drawSalesWindows()
 
     __beginDefaultProps("Sales Menu");
     __setWindowProps(winPosRatio, winSizeRatio);
-    
+
     ImVec2 displayPos(100, 100);
     for(int i = 0; i < 6; i++) {
         _addDisplayBeverage(displayPos + ImVec2((i % 3) * 220, (i / 3) * 150));
@@ -21,8 +21,6 @@ void GuiWrapper::drawSalesWindows()
     ImVec2 panelPos(100, 450);
     _addPaymentPanel(panelPos);
 
-
-    
     ImVec2 swapAdminPagePos(800, 80);
     ImGui::SetCursorPos(swapAdminPagePos);
     if (ImGui::Button("    change\nAdmin Page"))
@@ -61,7 +59,7 @@ void GuiWrapper::_addPaymentPanel(const ImVec2 &pos)
     };
     for (int i = 0; i < 5; i++)
         __addCoinButton(labels[i], pos + ImVec2((i % 3) * 150, (i / 3) * 100));
-    
+
     ImVec2 current(650, 450);
     ImGui::SetCursorPos(current);
     ImGui::Text("현재 잔액: ");
