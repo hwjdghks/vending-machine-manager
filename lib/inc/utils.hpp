503cefba4da5391fdafffa2a6386788ca7ce3cd1
hwjdghks <5832120@naver.com>
2024-06-13 02:51:28 +0900

    add Command utils

--- lib/inc/utils.hpp
+++ lib/inc/utils.hpp
@@ -1,16 +1,46 @@
 #ifndef UTILS_HPP
 #define UTILS_HPP
 
-#include <type_traits> /* static_assert */
 #include "imgui.h"
+#include <type_traits> /* static_assert */
+#include <string>
 
-#define DUMMY (void *)0
 #define BASIC_BUTTON_SIZE ImVec2(170, 60)
 
+/*
+ * 프로그램 창 그리기 모드
+ */
 enum class ViewMode {
-    SALES,
-    ADMIN
+    SALES,  // 판매
+    ADMIN   // 관리자
+};
+
+/*
+ * 서버와 클라이언트에서 사용되는 명령어 모음
+ * 화살표 시작: sender
+ * 화살표 끝: receiver
+ */
+enum class CMD {
+    WELCOME, // server --> client
+    BYE,    // server  --> client
+    UPDATE, // server <--> client
+    SELL,   // server <--  client
+    PRINT,  // server <--> client or server internal
+    _COUNT, // count of cmd list
+    NOMATCH // error
+};
+
+/*
+ * CMD에 해당하는 명령어 문자열 모음
+ */
+const std::string g_cmd_list[static_cast<int>(CMD::_COUNT)] = {
+    "WELCOME",
+    "BYE",
+    "UPDATE",
+    "SELL",
+    "PRINT"
 };
 
 ImVec2 getVec2(const ImVec2 &ratio);
+CMD parseFlow(const char *str);
 #endif
--- lib/src/utils.cpp
+++ lib/src/utils.cpp
@@ -1,9 +1,26 @@
 #include "utils.hpp"
 
+/*
+ * 현재 프로그램 창의 크기와 매개변수로 압력받은 비율을 곱한다
+ * 계산 결과를 바탕으로 벡터를 반환
+ */
 ImVec2 getVec2(const ImVec2 &ratio)
 {
     ImVec2 display = ImGui::GetIO().DisplaySize;
     int x = static_cast<int>(display.x * ratio.x);
     int y = static_cast<int>(display.y * ratio.y);
     return ImVec2(x, y);
+}
+
+/*
+ * 문자열이 기존 명령어 리스트에 있는지 검사
+ * 일치시 인덱스에 해당하는 enum class 반환
+ * 불일치시 NOMATCH 반환
+ */
+CMD parseFlow(const std::string &str)
+{
+    for (int i = 0; i < static_cast<int>(CMD::_COUNT); i++)
+        if (str == g_cmd_list[i])
+            return static_cast<CMD>(i);
+    return CMD::NOMATCH;
 }
